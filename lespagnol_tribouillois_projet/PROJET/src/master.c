bd3d4e4628c20ecb5d2379c28220dbf0b15381fd
Zaiphyr <julientribouillois37240@gmail.com>
2022-11-14 11:42:26 +0100

    rgvgvdfvf

--- lespagnol_tribouillois_projet/PROJET/src/master.c
+++ lespagnol_tribouillois_projet/PROJET/src/master.c
@@ -39,8 +39,17 @@ void loop(/* paramètres */)
 {
     // boucle infinie :
     // - ouverture des tubes (cf. rq client.c)
+    int master_client = open("master_client", O_WRONLY);
+    int client_master = open("client_master", O_RDONLY);
+    
+    int commande;
+    int nbr_test;
     // - attente d'un ordre du client (via le tube nommé)
     // - si ORDER_STOP
+    if(commande == -1)
+    {
+        // demader au worker de se fermer
+    }
     //       . envoyer ordre de fin au premier worker et attendre sa fin
     //       . envoyer un accusé de réception au client
     // - si ORDER_COMPUTE_PRIME
@@ -52,11 +61,26 @@ void loop(/* paramètres */)
     //       . envoyer N dans le pipeline
     //       . récupérer la réponse
     //       . la transmettre au client
+    if(commande == 1)
+    {
+
+    }
     // - si ORDER_HOW_MANY_PRIME
     //       . transmettre la réponse au client
+    if(// à chaque fois qu'un worker renvoie true
+    {
+    	nb_prime++;
+    }
     // - si ORDER_HIGHEST_PRIME
     //       . transmettre la réponse au client
+    int max = 0;
+    if(max < result)
+    {
+    	max = result;
+    }
     // - fermer les tubes nommés
+    close(master_client);
+    close(client_master);
     // - attendre ordre du client avant de continuer (sémaphore : précédence)
     // - revenir en début de boucle
     //
@@ -71,17 +95,36 @@ void loop(/* paramètres */)
 
 int main(int argc, char * argv[])
 {
-    if (argc != 1)
+     if (argc != 1)
         usage(argv[0], NULL);
 
     // - création des sémaphores
+    int sema1, sema2;
+    
+    sema1 = semget(, 2, IPC_CREAT | 0641);
+    assert(sema1 != -1);
+    
+    sema2 = semget(, 2, IPC_CREAT | 0641);
+    assert(sema2 != -1);
+    
     // - création des tubes nommés
+    int ret1 = mkfifo("master_client", 0644);
+    assert(ret1 == 0);
+    int ret2 = mkfifo("client_master", 0644);
+    assert(ret2 == 0);
+    
     // - création du premier worker
+
 
     // boucle infinie
     loop(/* paramètres */);
 
     // destruction des tubes nommés, des sémaphores, ...
+    
+    ret1 = unlink("master_client");
+    assert(ret1 == 0);
+    ret2 = unlink("client_master");
+    assert(ret2 == 0);
 
     return EXIT_SUCCESS;
 }
